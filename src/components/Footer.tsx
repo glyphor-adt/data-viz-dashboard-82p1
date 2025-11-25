961584fd8d5a97bb4eeb21e9fc523da508cd8e29
glyphor-adt <Kristina@Glyphorai.onmicrosoft.com>
2025-11-24 20:20:43 -0600

    Add src/components/Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="bg-gray-900 text-gray-400 py-8">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2024 Data Visualization Dashboard. All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
